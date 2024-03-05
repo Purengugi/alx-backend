4d15757dcee18aa2bb3513b2ee57649723b1284d
Purengugi <purity2004ngugi@gmail.com>
2024-03-05 15:09:53 +0000

    update

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,38 +1,61 @@
-#!/usr/bin/python3
-""" 100-lfu_cache """
+#!/usr/bin/env python3
+"""defines a subclass LFUCache"""
 from base_caching import BaseCaching
+from collections import defaultdict, OrderedDict
 
 
 class LFUCache(BaseCaching):
-    """ LFUCache class that inherits from BaseCaching
-    """
-
+    """inherits from super and defines a caching system"""
     def __init__(self):
-        """ Initialize LFUCache """
+        """initializes the class"""
         super().__init__()
-        self.freq_count = {}
+        self.cache_data = {}
+        self.frequency = defaultdict(int)
+        self.usage_order = OrderedDict()
 
     def put(self, key, item):
-        """ Assign item to key in self.cache_data """
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                self.cache_data[key] = item
-                self.freq_count[key] += 1
-            else:
-                if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    min_freq_keys = [k for k, v in self.freq_count.items() if v == min(self.freq_count.values())]
-                    lru_key = min(min_freq_keys, key=lambda x: self.cache_data[x])
-                    del self.cache_data[lru_key]
-                    del self.freq_count[lru_key]
-                    print("DISCARD: {}".format(lru_key))
-                self.cache_data[key] = item
-                self.freq_count[key] = 1
-        else:
+        """assigns the value to the key provided.
+        Also defines a LFU caching system to deal with
+        the capicty of the cahce"""
+        if key is None or item is None:
             return
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.frequency[key] += 1
+            self.usage_order.move_to_end(key)
+        else:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                self.remove_lfu()
+
+            self.cache_data[key] = item
+            self.frequency[key] = 1
+            self.usage_order[key] = None
 
     def get(self, key):
-        """ Return the value in self.cache_data linked to key """
-        if key is not None and key in self.cache_data:
-            self.freq_count[key] += 1
-            return self.cache_data[key]
-        return None
+        """retrieves value associated with key whilst
+        also update the frequency in"""
+        if key is None or key not in self.cache_data:
+            return None
+        self.frequency[key] += 1
+        self.usage_order.move_to_end(key)
+        return self.cache_data[key]
+
+    def remove_lfu(self):
+        """discards the data with the least frequency of usage
+        from the caching system"""
+        min_freq = min(self.frequency.values())
+        min_f = [k for k, v in self.frequency.items() if v == min_freq]
+        if len(min_f) > 1:
+            for key in self.usage_order:
+                if key in min_f:
+                    del self.cache_data[key]
+                    del self.frequency[key]
+                    del self.usage_order[key]
+                    print(f"DISCARD: {key}")
+                    break
+        else:
+            key_to_remove = min_f[0]
+            del self.cache_data[key_to_remove]
+            del self.frequency[key_to_remove]
+            del self.usage_order[key_to_remove]
+            print(f"DISCARD: {key_to_remove}")
