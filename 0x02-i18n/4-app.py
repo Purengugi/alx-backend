d9cb4b80310c77ef46e47eea248044aca2a8d283
Purengugi <purity2004ngugi@gmail.com>
2024-03-12 16:09:41 +0000

    updae

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,22 +1,15 @@
 #!/usr/bin/env python3
-"""
-Flask app
-"""
-from flask import (
-    Flask,
-    render_template,
-    request
-)
+"""Force locale with URL parameter"""
+
+from flask import Flask, request, render_template
 from flask_babel import Babel
 
 
 class Config(object):
-    """
-    Configuration for Babel
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    """Flask app config"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
@@ -24,24 +17,23 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@app.route("/")
+def index() -> str:
+    """Return 4-index.html"""
+    return render_template("4-index.html")
+
+
 @babel.localeselector
-def get_locale():
-    """
-    Select and return best language match based on supported languages
-    """
-    loc = request.args.get('locale')
-    if loc in app.config['LANGUAGES']:
-        return loc
+def get_locale() -> str:
+    """Determing preferred locale"""
+    locale = request.args.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
-    """
-    Handles / route
-    """
-    return render_template('4-index.html')
+# babel.init_app(app, locale_selector=get_locale)
 
 
 if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+    app.run()
