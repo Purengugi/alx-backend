301d05ed5d37b7be897d817230433b0be3f7164b
Purengugi <purity2004ngugi@gmail.com>
2024-03-12 16:02:34 +0000

    update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
-"""basic flask setup"""
+"""
+Flask app
+"""
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    """displays a simple index with some text"""
-    greeting = "Hello world"
-    heading = "Welcome to Holberton"
-    return render_template('1-index.html', greeting=greeting, heading=heading)
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / route
+    """
+    return render_template('0-index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
